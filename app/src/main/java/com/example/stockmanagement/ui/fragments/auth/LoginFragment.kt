48a56fb86158855f1cc3ab46e59d8f580a5e97e1
prinzz1208 <priyanshu81212@gmail.com>
2021-02-27 14:07:42 +0530

    Login And Signup Fragments creation complete

--- app/src/main/java/com/example/stockmanagement/ui/fragments/auth/LoginFragment.kt
+++ app/src/main/java/com/example/stockmanagement/ui/fragments/auth/LoginFragment.kt
@@ -1,17 +1,42 @@
 package com.example.stockmanagement.ui.fragments.auth
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.stockmanagement.R
+import com.example.stockmanagement.databinding.FragmentLoginBinding
+import dagger.hilt.android.AndroidEntryPoint
 
 
 class LoginFragment : Fragment(R.layout.fragment_login) {
 
-  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-    super.onViewCreated(view, savedInstanceState)
+    private lateinit var binding: FragmentLoginBinding
 
-  }
+    override fun onCreateView(
+      inflater: LayoutInflater,
+      container: ViewGroup?,
+      savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init()
+    }
+
+    private fun init() {
+        setUpListeners()
+    }
+
+    private fun setUpListeners() {
+        binding.loginSignupButton.setOnClickListener {
+            findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToSignupFragment())
+        }
+    }
+
 }
